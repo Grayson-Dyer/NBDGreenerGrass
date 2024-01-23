3272ab0174a2a09c3ab21d59a9dc51c04018730d
Grayson Dyer <gdyer1@ncstudents.niagaracollege.ca>
2024-01-23 18:12:25 -0500

    Test Push to main

--- NBDGreenerGrass/Models/ErrorViewModel.cs
+++ NBDGreenerGrass/Models/ErrorViewModel.cs
@@ -3,7 +3,8 @@ namespace NBDGreenerGrass.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
+        
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        //Test Push
     }
 }
