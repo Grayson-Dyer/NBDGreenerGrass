982cd1faf48ce1fbcd2a0070a3eb1e4822201af1
aansonFindlater <afindlater1@ncstudents.niagaracollege.ca>
2024-02-22 01:06:24 -0500

    Update Inventory.cs

--- NBDGreenerGrass/Models/Inventory.cs
+++ NBDGreenerGrass/Models/Inventory.cs
@@ -7,26 +7,26 @@ namespace NBDGreenerGrass.Models
         
         public int ID { get; set; }
 
-        //TODO: Shouldnt there be an inventory name? = Cross ref the model with the excell we use Code & Desc for that.
+        //TODO: Shouldnt there be an inventory name? = Cross ref the model with the excel we use Code & Desc for that.
         [Display(Name = "Inventory Desc")]
         public string InventoryDesc { get; set; }
 
-        // TODO: Should this be a string or a number? = Look at the model, it's a float(8,2) 
+        // TODO: Should this be a string or a number? = In the model, it's a float(8,2) 
         [Display(Name = "Inventory Size")]
         [Required(ErrorMessage = "Inventory Size is required")]
         [RegularExpression(@"^\d{1,6}(\.\d{1,2})?$", ErrorMessage = "Invalid format. Maximum 999999.99")]
         public decimal? InventorySize { get; set; }
-
-        [Display(Name = "Inventory Code")]
-        [Required(ErrorMessage = "Inventory Code is required")]
-        [StringLength(50, ErrorMessage = "Inventory Code should be at most 50 characters")]
-        public string InventoryCode { get; set; }
 
         [Display(Name = "Inventory List Price")]
         [DataType(DataType.Currency)]
         [Required(ErrorMessage = "Inventory List Price is required")]
         [Range(0, double.MaxValue, ErrorMessage = "Inventory List Price must be greater than or equal to 0")]
         public double InventoryListPrice { get; set; } // This was a decimal in the original code but I changed it to a double to match the other BidMaterial properties
+
+        [Display(Name = "Inventory Code")]
+        [Required(ErrorMessage = "Inventory Code is required")]
+        [StringLength(50, ErrorMessage = "Inventory Code should be at most 50 characters")]
+        public string InventoryCode { get; set; }
 
 
         public ICollection<BidMaterial> BidMaterials { get; set; }
