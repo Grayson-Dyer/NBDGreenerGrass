312fc820921a479a1c95b1fc181793b151be0bd6
aansonFindlater <afindlater1@ncstudents.niagaracollege.ca>
2024-02-22 01:30:54 -0500

    Update Inventory.cs

--- NBDGreenerGrass/Models/Inventory.cs
+++ NBDGreenerGrass/Models/Inventory.cs
@@ -17,19 +17,17 @@ namespace NBDGreenerGrass.Models
         [Required(ErrorMessage = "Inventory Size is required")]
         public string InventorySize { get; set; }
 
-        [Display(Name = "Inventory List Price")]
-        [DataType(DataType.Currency)]
-        [Required(ErrorMessage = "Inventory List Price is required")]
-        [Range(0, double.MaxValue, ErrorMessage = "Inventory List Price must be greater than or equal to 0")]
-        [RegularExpression(@"^\d{1,6}(\.\d{1,2})?$", ErrorMessage = "Invalid format. Maximum 999999.99")]
-        public decimal InventoryListPrice { get; set; }
-        // This was a decimal in the original code but I changed it to a double to match the other BidMaterial properties = changed back for precision / also changed in BidMaterial 
-
         [Display(Name = "Inventory Code")]
         [Required(ErrorMessage = "Inventory Code is required")]
         [StringLength(50, ErrorMessage = "Inventory Code should be at most 50 characters")]
         public string InventoryCode { get; set; }
 
+        [Display(Name = "Inventory List Price")]
+        [DataType(DataType.Currency)]
+        [Required(ErrorMessage = "Inventory List Price is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Inventory List Price must be greater than or equal to 0")]
+        [RegularExpression(@"^\d{1,6}(\.\d{1,2})?$", ErrorMessage = "Invalid format. Maximum 999999.99")]
+        public decimal InventoryListPrice { get; set; } // This was a decimal in the original code but I changed it to a double to match the other BidMaterial properties = changed back for precision / also changed in BidMaterial 
 
         public ICollection<BidMaterial> BidMaterials { get; set; }
 
