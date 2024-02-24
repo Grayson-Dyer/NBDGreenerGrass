588576eb3f846461b909898543401f77ca600435
aansonFindlater <afindlater1@ncstudents.niagaracollege.ca>
2024-02-23 21:27:30 -0500

    Update BidStaff.cs

--- NBDGreenerGrass/Models/BidStaff.cs
+++ NBDGreenerGrass/Models/BidStaff.cs
@@ -1,21 +1,3 @@
 
 
 
-
-// we get rid of this here class
-
-using System.ComponentModel.DataAnnotations;
-namespace NBDGreenerGrass.Models
-{
-    public class BidStaff
-    {
- 
-        [Required(ErrorMessage = "Project ID is required")]
-        public int ProjectID { get; set; }
-        public Project Project { get; set; }
-
-        [Required(ErrorMessage = "Staff ID is required")]
-        public int StaffID { get; set; }
-        public Staff Staff { get; set; }
-    }
-}
