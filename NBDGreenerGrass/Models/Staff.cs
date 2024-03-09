3b8e8c595dc7b2d881bbb5df3a27b863bd435e16
aansonFindlater <afindlater1@ncstudents.niagaracollege.ca>
2024-03-09 17:15:56 -0500

    Update Staff.cs

--- NBDGreenerGrass/Models/Staff.cs
+++ NBDGreenerGrass/Models/Staff.cs
@@ -6,17 +6,17 @@ namespace NBDGreenerGrass.Models
     {
         public int ID { get; set; }
 
-        [Display(Name = "First Name")]
+        [Display(Name = "First Name", Description = "Enter the first name of the staff")]
         [Required(ErrorMessage = "First Name is required")]
         [StringLength(50, ErrorMessage = "First Name should be at most 50 characters")]
         public string StaffFirst { get; set; }
 
-        [Display(Name = "Last Name")]
+        [Display(Name = "Last Name", Description = "Enter the last name of the staff")]
         [Required(ErrorMessage = "Last Name is required")]
         [StringLength(50, ErrorMessage = "Last Name should be at most 50 characters")]
         public string StaffLast { get; set; }
 
-        [Display(Name = "Role")]
+        [Display(Name = "Role", Description = "Select the role of the staff")]
         [Required(ErrorMessage = "Staff Role is required")]
         public int StaffRoleID { get; set; }
 
