66889ef3bc56433e27b9b29252a1574c1b0f766b
aansonFindlater <afindlater1@ncstudents.niagaracollege.ca>
2024-03-09 18:40:18 -0500

    Update BidMaterial.cs

--- NBDGreenerGrass/Models/BidMaterial.cs
+++ NBDGreenerGrass/Models/BidMaterial.cs
@@ -10,37 +10,37 @@ namespace NBDGreenerGrass.Models
         //TODO: Is this a composite key?
         public int ID { get; set; }
 
-        [Display(Name = "Bid", Description = "Select the bid associated with this entry")]
+        [Display(Name = "Bid")]
         [Required(ErrorMessage = "Bid ID is required")]
         public int BidID { get; set; }
 
         [Display(Name = "Bid")]
         public Bid Bid { get; set; }
 
-        [Display(Name = "Inventory", Description = "Select the inventory associated with this entry")]
+        [Display(Name = "Inventory")]
         [Required(ErrorMessage = "Inventory ID is required")]
         public int InventoryID { get; set; }
 
         [Display(Name = "Inventory")]  
         public Inventory Inventory { get; set; }
 
-        [Display(Name = "Inventory Description", Description = "Enter a brief description of the inventory")]
+        [Display(Name = "Inventory Description")]
         [StringLength(75, ErrorMessage = "Inventory Description should be at most 75 characters")]
         [Required(ErrorMessage = "Inventory Description is required")]
         public string InventoryDesc { get; set; }
 
         //TODO: is this a string or a number??? = Looks like booth see note below 
-        [Display(Name = "Inventory Size", Description = "Enter the size of the inventory")]
+        [Display(Name = "Inventory Size")]
         [StringLength(50, ErrorMessage = "Inventory Size should be at most 50 characters")]
         [Required(ErrorMessage = "Inventory Size is required")]
         public string InventorySize { get; set; }
 
-        [Display(Name = "Inventory Code", Description = "Enter the code associated with the inventory")]
+        [Display(Name = "Inventory Code")]
         [StringLength(50, ErrorMessage = "Inventory Code should be at most 50 characters")]
         [Required(ErrorMessage = "Inventory Code is required")]
         public string InventoryCode { get; set; }
 
-        [Display(Name = "Inventory List Price", Description = "Enter the list price of the inventory")]
+        [Display(Name = "Inventory List Price")]
         [DataType(DataType.Currency)]
         [Required(ErrorMessage = "Inventory List Price is required")]
         [Range(0, double.MaxValue, ErrorMessage = "Inventory List Price must be greater than or equal to 0")]
