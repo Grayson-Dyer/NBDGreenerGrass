29c058e952912d30de5ba19e2eefbbc1eec9eab7
aansonFindlater <afindlater1@ncstudents.niagaracollege.ca>
2024-03-09 17:12:41 -0500

    Update Labour.cs

--- NBDGreenerGrass/Models/Labour.cs
+++ NBDGreenerGrass/Models/Labour.cs
@@ -6,17 +6,17 @@ namespace NBDGreenerGrass.Models
     {
         public int ID { get; set; }
 
-        [Display(Name = "Labour Type")]
+        [Display(Name = "Labour Type", Description = "Enter the type of labor")]
         [Required(ErrorMessage = "Labour Type is required")]
         public string LabourType { get; set; }
 
-        [Display(Name = "Labour Price")]
+        [Display(Name = "Labour Price", Description = "Enter the price for the labor")]
         [Required(ErrorMessage = "Labour Price is required")]
         [DataType(DataType.Currency)]
         [RegularExpression(@"^\d{1,6}(\.\d{1,2})?$", ErrorMessage = "Invalid format. Maximum 999999.99")]
         public decimal LabourPrice { get; set; }
 
-        [Display(Name = "Labour Cost")]
+        [Display(Name = "Labour Cost", Description = "Enter the cost for the labor")]
         [Required(ErrorMessage = "Labour Cost is required")]
         [DataType(DataType.Currency)]
         [RegularExpression(@"^\d{1,6}(\.\d{1,2})?$", ErrorMessage = "Invalid format. Maximum 999999.99")]
