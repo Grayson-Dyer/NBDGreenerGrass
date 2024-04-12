124937751522ba125776eeecf7df02daace61f19
Nzekwesi Uju Amanda Rodrigues <unzekwesi1@ncstudents.niagaracollege.ca>
2024-04-12 16:21:15 -0400

    A

--- NBDGreenerGrass/Controllers/ProjectController.cs
+++ NBDGreenerGrass/Controllers/ProjectController.cs
@@ -126,12 +126,13 @@ namespace NBDGreenerGrass.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "Management,Designer,Sales")]
-        public async Task<IActionResult> Create([Bind("ID,Start,End,Amount,Created,Street,City,Province,Postal,Desc,ClientID")] Project project)
+        public async Task<IActionResult> Create([Bind("ID,Start,End,Amount,Street,City,Province,Postal,Desc,ClientID")] Project project)
         {
             try
             {
                 if (ModelState.IsValid)
                 {
+                    project.Created = DateTime.Now;
                     _context.Add(project);
                     await _context.SaveChangesAsync();
                     return RedirectToAction(nameof(Index));
@@ -170,7 +171,7 @@ namespace NBDGreenerGrass.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "Management,Designer,Sales")]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Start,End,Amount,Created,Street,City,Province,Postal,Desc,ClientID")] Project project)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Start,End,Amount,Street,City,Province,Postal,Desc,ClientID")] Project project)
         {
             if (id != project.ID)
             {
